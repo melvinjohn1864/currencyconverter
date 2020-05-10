98a8bfea8f3bf8da3d214f32a03769b65be5e066
Melvin <melvinjohn1864@gmail.com>
2020-05-10 18:32:32 -0400

    Attach the view binding class to Login Activity

--- app/src/main/java/com/example/mmconverter/LoginActivity.kt
+++ app/src/main/java/com/example/mmconverter/LoginActivity.kt
@@ -2,11 +2,16 @@ package com.example.mmconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mmconverter.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
