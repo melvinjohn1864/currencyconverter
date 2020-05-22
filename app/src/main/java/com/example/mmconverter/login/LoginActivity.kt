6ef9904822d80435cf5fc196db18a0c3f46a74f6
Melvin <melvinjohn1864@gmail.com>
2020-05-22 18:22:00 -0400

    Create the view binding functions and parameters in activity

--- app/src/main/java/com/example/mmconverter/login/LoginActivity.kt
+++ app/src/main/java/com/example/mmconverter/login/LoginActivity.kt
@@ -2,7 +2,10 @@ package com.example.mmconverter.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import com.example.mmconverter.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,5 +16,11 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val loginViewModel: LoginViewModel by viewModels()
+
+        binding.btnLogin.setOnClickListener {
+            loginViewModel.checkLoginCredentials(binding.etUsername.toString(),binding.etPassword.toString())
+        }
     }
 }
